cdebbc5f338cacf2c9531321ae77338dfb95d291
Vishal Menon <vishalmenon.92@gmail.com>
2018-10-07 16:49:00 +0530

    Update TxParser.js

--- TxParser.js
+++ TxParser.js
@@ -23,7 +23,7 @@ var app = express();
 
 app.use(helmet());
 app.use(helmet.noCache());
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 var rec_set = {
