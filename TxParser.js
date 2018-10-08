f4f215892ea635dbb7648d13fcd8224268a27e2a
Vishal Menon <vishalmenon.92@gmail.com>
2018-10-08 08:49:22 +0530

    Update TxParser.js

--- TxParser.js
+++ TxParser.js
@@ -61,7 +61,7 @@ return new Promise((resolve,reject)=>{
   request.post({
    "headers":{ "content-type": "application/JSON" },
    "url": `${server_url}/tx_detail_local`,
-   "body":{"txid":txid}
+   "body":JSON.parse({"txid":txid})
   },
    (error, response, body)=>{
     if(error){
